7f5044614769179068d1b4df0fb6ffdfff148d30
anuroopmallick <anuroopmallick1@gmail.com>
2022-01-05 10:53:51 +0530

    no changes done

---
+++ htmlpriject.js
@@ -0,0 +1,228 @@
+
+$(document).ready(()=>
+{
+    // $("#myinput").focus(function()
+    // {
+    //     $("h1").css("color","blue")
+    //     $("h1").hide(3000)
+    // })
+
+    // $("#myinput").blur(function()
+    // {
+    //     $("h1").show(3000)
+    // })
+
+
+    // $("#mydiv").scroll(function()
+    // {
+    //     $("h1").CSS("color","blue")
+    // })
+
+    // $("#myinput").keyup(function()   //keydown
+    // {
+    //     $("h1").hide(3000)
+    // })
+
+    // $("button").click(function()
+    // {
+    //     $("h1").hide(3000, function()
+    //     {
+    //         $("h2").css("color","red")
+    //     })
+    // })
+
+    // $("button").click(function()   
+    // {
+    //      $("h2").fadeOut(3000)
+    // })
+    // $("button").click(function()   
+    // {
+    //      $("h2").fadeIn(3000)
+    // })
+    // $("button").click(function()   
+    // {
+    //      $("h2").fadeToggle(3000) 
+    // })
+    // $("button").click(function()   
+    // {
+    //      $("h2").fadeTo(3000, 0.3) 
+    // })
+
+    //click, slideUp,slideDown
+
+
+    
+
+    // Jquery -> chaining, avoids code repetation
+
+    // $("button").click(()=>
+    // {
+    //     $("h2").css("background-color","red").fadeOut(3000).fadeIn(4000)
+    //     $("#mydiv").slideUp(3000).slideDown(3000)
+    // })
+
+    // jquery animate
+
+    // $("button").click(()=>
+    {
+    //     $("#div2").animate({height: "600px", opacity: "0.4"}, 2000)
+    //     $("#div2").animate({width: "600px"}, 2000).animate({height: "150px",opacity: "1"}, 2000).animate({height: "150px"}, 1000).animate({width: "150px"}, 1000)
+    
+    
+    //thursday lectures -> get from sir
+    
+    
+        //    let width = $("div").width() // only content, no padding, no border
+        //    alert(width)
+
+        //    let height = $("div").height() // only content, no padding, no border
+        //    alert(height)   
+
+        //    let width = $("div").innerWidth() // both conten and padiing, but no border
+        //    let height = $("div").outerWidth() // both conten and padiing, but no border
+        //    let height = $("div").outerHeight() // both conten and padiing, but no border
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
